4cf972835c845d51352ff28d349583c61dbdcdf3
Faiq Rosadi Arridho <68257060+Faiqrosadi@users.noreply.github.com>
2024-07-14 15:46:19 +0000

    others done

--- src/generator.py
+++ src/generator.py
@@ -217,7 +217,70 @@ def build_annual_pages(start_year, end_year, j2_env: j2.Environment):
     )
   return annual_templates
 
+#OTHER PAGES
+def build_other_pages(start:date, j2_env: j2.Environment):
+  def build_other_page(j2_template: j2.Template):
+    return j2_template.render()
 
+  sidebar_bulan_list = []
+  for i in range(12):
+    sidebar_bulan_list.append(mini_calendar_dates(i + 1, start.year))
+
+  def build_frame(name:str, template:j2.Template):
+    content = build_other_page(template)
+    annual_templates[name] = frame_template.render(
+      content=content,
+      index_page=False,
+      sidebar_bulan_list=sidebar_bulan_list
+    )
+
+  annual_templates = {}
+  skincare_template = j2_env.get_template('other_skincare.html.j2')
+  cleaning_template = j2_env.get_template('other_cleaning.html.j2')
+  project_template = j2_env.get_template('other_project.html.j2')
+  password_template = j2_env.get_template('other_password.html.j2')
+  habit_template = j2_env.get_template('other_habit.html.j2')
+  account_template = j2_env.get_template('other_account.html.j2')
+  album1_template = j2_env.get_template('other_album1.html.j2')
+  album2_template = j2_env.get_template('other_album2.html.j2')
+  album3_template = j2_env.get_template('other_album3.html.j2')
+  tl1_template = j2_env.get_template('other_tl1.html.j2')
+  tl2_template = j2_env.get_template('other_tl2.html.j2')
+  tl3_template = j2_env.get_template('other_tl3.html.j2')
+  tl4_template = j2_env.get_template('other_tl4.html.j2')
+  tl5_template = j2_env.get_template('other_tl5.html.j2')
+  tl6_template = j2_env.get_template('other_tl6.html.j2')
+  tl7_template = j2_env.get_template('other_tl7.html.j2')
+  tl8_template = j2_env.get_template('other_tl8.html.j2')
+  tl9_template = j2_env.get_template('other_tl9.html.j2')
+  frame_template = j2_env.get_template('frame.html.j2')
+  
+ 
+  build_frame('skincare_page', skincare_template)
+  build_frame('cleaning_page', cleaning_template)
+  for i in range(10):
+    content = project_template.render(id='pp'+str(i+1))
+    annual_templates['pp'+str(i)] = frame_template.render(
+      content=content,
+      index_page=False,
+      sidebar_bulan_list=sidebar_bulan_list
+    )
+  build_frame('password_page', password_template)
+  build_frame('habit_page', habit_template)
+  build_frame('account-page', account_template)
+  build_frame('album1-page', album1_template)
+  build_frame('album2-page', album2_template)
+  build_frame('album3-page', album3_template)
+  build_frame('tl8-page', tl8_template)
+  build_frame('tl1-page', tl1_template)
+  build_frame('tl2-page', tl2_template)
+  build_frame('tl3-page', tl3_template)
+  build_frame('tl4-page', tl4_template)
+  build_frame('tl5-page', tl5_template)
+  build_frame('tl6-page', tl6_template)
+  build_frame('tl7-page', tl7_template)
+  build_frame('tl9-page', tl9_template)
+  return annual_templates
 
 if __name__ == "__main__":
   parser = ArgumentParser(prog='Python Planner Generator',
@@ -246,6 +309,7 @@ if __name__ == "__main__":
   pages = []
 
   pages.extend(build_annual_pages(start_date, end_date, env).values())
+  pages.extend(build_other_pages(start_date, env).values())
   # pages.extend(build_monthly_pages(start_date, end_date, env).values())
 
   # if args.weekly_pages:
